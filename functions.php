41165b98aacd91903675a7a2d2ae0f0fc25e39dc
efuller <eric@alley.co>
2023-11-30 14:24:20 -0500

    Create, not Custom

--- functions.php
+++ functions.php
@@ -7,8 +7,8 @@
 
 namespace Create_WordPress_Theme;
 
-define( 'CUSTOM_WORDPRESS_THEME_PATH', __DIR__ );
-define( 'CUSTOM_WORDPRESS_URL', get_template_directory_uri() );
+define( 'CREATE_WORDPRESS_THEME_PATH', __DIR__ );
+define( 'CREATE_CUSTOM_WORDPRESS_URL', get_template_directory_uri() );
 
 // Theme customizer.
-require_once CUSTOM_WORDPRESS_THEME_PATH . '/inc/customizer.php';
+require_once CREATE_WORDPRESS_THEME_PATH . '/inc/customizer.php';
