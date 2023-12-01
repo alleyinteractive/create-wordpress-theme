7021f8725b266cfe2ac2f40c8070f3bd469c23ba
efuller <eric@alley.co>
2023-12-01 12:07:20 -0500

    Rename constant

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
 namespace Create_WordPress_Theme;
 
 define( 'CREATE_WORDPRESS_THEME_PATH', __DIR__ );
-define( 'CREATE_CUSTOM_WORDPRESS_URL', get_template_directory_uri() );
+define( 'CREATE_WORDPRESS_THEME_URL', get_template_directory_uri() );
 
 // Theme setup.
 require_once CREATE_WORDPRESS_THEME_PATH . '/inc/theme.php';
