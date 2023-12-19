4fb5045b9d27d7bab1f1459b4799573f5b759537
efuller <eric@alley.co>
2023-12-19 15:57:35 -0500

    Add in the Customize menu item to the Appearance menu

--- inc/theme.php
+++ inc/theme.php
@@ -8,6 +8,7 @@
 namespace Create_WordPress_Theme;
 
 add_action( 'after_setup_theme', __NAMESPACE__ . '\action__after_theme_setup' );
+add_action( 'admin_menu', __NAMESPACE__ . '\action__admin_menu' );
 
 /**
  * Setup theme defaults and registers support for various WordPress features.
@@ -17,9 +18,25 @@ function action__after_theme_setup(): void {
 	 * Add menu support.
 	 */
 	add_theme_support( 'menus' );
+}
 
-	/**
-	 * Enable theme customizer.
-	 */
-	add_action( 'customize_register', '__return_true' );
+/**
+ * Add in the Customize link to the Appearance menu.
+ *
+ * @return void
+ */
+function action__admin_menu(): void {
+	// Build the customize.php URL.
+	$current_url   = esc_url_raw( wp_unslash( $_SERVER['REQUEST_URI'] ) );
+	$customize_url = add_query_arg( 'return', urlencode( remove_query_arg( wp_removable_query_args(), $current_url ) ), 'customize.php' );
+
+	add_submenu_page(
+		'themes.php',
+		__( 'Customize', 'create-wordpress-theme' ),
+		__( 'Customize', 'create-wordpress-theme' ),
+		'customize',
+		esc_url( $customize_url ),
+		'',
+		60
+	);
 }
