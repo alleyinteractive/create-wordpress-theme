4622c5099ee3a3d21b71e399506203761e2082a6
efuller <eric@alley.co>
2023-12-19 14:15:02 -0500

    Refactor site editor access
    - Instead of rebuilding the appearance menu, just remove the Editor menu
    - Deny edit_theme_options capabilities to all and re-apply if not on site-editor.php

--- inc/site-editor.php
+++ inc/site-editor.php
@@ -24,20 +24,11 @@ function action__after_setup_theme(): void {
 		}
 
 		Caper::deny_to_all()->primitives( 'edit_theme_options' );
-		// Create a custom capability for menu items that should be displayed.
-		Caper::grant_to( 'administrator' )->primitives( 'create_wordpress_theme_edit_menus' );
 
-		// Conditionally re-grant access to the customizer.
-		if ( ( 'customize.php' === $pagenow || 'index.php' === $pagenow ) || wp_doing_ajax() ) {
+		if ( 'site-editor.php' !== $pagenow ) {
 			Caper::grant_to( 'administrator' )->primitives( 'edit_theme_options' )->at_priority( 100 );
 		}
 
-		// Conditionally re-grant access to menus page.
-		if ( 'nav-menus.php' === $pagenow || wp_doing_ajax() ) {
-			Caper::grant_to( 'administrator' )->primitives( 'edit_theme_options' )->at_priority( 100 );
-		}
-
-		// Rebuild the appearance menu.
 		add_action( 'admin_menu', __NAMESPACE__ . '\action__admin_menu' );
 	}
 }
@@ -46,45 +37,5 @@ function action__after_setup_theme(): void {
  * Remove and rebuild the appearance menu.
  */
 function action__admin_menu(): void {
-	// Build the customize.php URL.
-	$current_url   = esc_url_raw( wp_unslash( $_SERVER['REQUEST_URI'] ) );
-	$customize_url = add_query_arg( 'return', urlencode( remove_query_arg( wp_removable_query_args(), $current_url ) ), 'customize.php' );
-
-	// Remove the "Appearance" menu items.
-	remove_menu_page( 'themes.php' );
 	remove_submenu_page( 'themes.php', 'site-editor.php' );
-	remove_submenu_page( 'themes.php', $customize_url );
-	remove_submenu_page( 'themes.php', 'nav-menus.php' );
-
-	// Rebuild the Appearance menu.
-	add_menu_page(
-		__( 'Appearance', 'create-wordpress-theme' ),
-		__( 'Appearance', 'create-wordpress-theme' ),
-		'create_wordpress_theme_edit_menus',
-		'themes.php',
-		// @phpstan-ignore-next-line because the issue is with the docblock in WP core; we are passing the correct default value.
-		'',
-		'dashicons-admin-appearance',
-		60 // Default themes.php menu item position.
-	);
-
-	add_submenu_page(
-		'themes.php',
-		__( 'Customize', 'create-wordpress-theme' ),
-		__( 'Customize', 'create-wordpress-theme' ),
-		'create_wordpress_theme_edit_menus',
-		esc_url( $customize_url ),
-		'',
-		61
-	);
-
-	add_submenu_page(
-		'themes.php',
-		__( 'Menus', 'create-wordpress-theme' ),
-		__( 'Menus', 'create-wordpress-theme' ),
-		'create_wordpress_theme_edit_menus',
-		'nav-menus.php',
-		'',
-		62
-	);
 }
